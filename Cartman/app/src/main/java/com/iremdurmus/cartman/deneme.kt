67f8cd7ae4172ea8f3c55f98a7ea74d76cab52a4
iremdrms <durmus.irem@outlook.com>
2024-04-17 09:38:23 +0300

    Project created

---
+++ Cartman/app/src/main/java/com/iremdurmus/cartman/deneme.kt
@@ -0,0 +1,11 @@
+package com.iremdurmus.cartman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class deneme : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deneme)
+    }
+}
