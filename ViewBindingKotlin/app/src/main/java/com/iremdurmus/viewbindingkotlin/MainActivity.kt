714b685003f17f4bb4172c3d6b85e763a6b185fe
iremdrms <durmus.irem@outlook.com>
2024-04-24 10:25:07 +0300

    .

--- ViewBindingKotlin/app/src/main/java/com/iremdurmus/viewbindingkotlin/MainActivity.kt
+++ ViewBindingKotlin/app/src/main/java/com/iremdurmus/viewbindingkotlin/MainActivity.kt
@@ -2,10 +2,18 @@ package com.iremdurmus.viewbindingkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.iremdurmus.viewbindingkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
     }
 }
