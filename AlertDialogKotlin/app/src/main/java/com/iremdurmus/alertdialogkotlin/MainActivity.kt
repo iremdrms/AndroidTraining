d413a2baaaab0c9dca02a2b3d6bd8033b10514ee
iremdrms <durmus.irem@outlook.com>
2024-04-29 13:55:08 +0300

    .

--- AlertDialogKotlin/app/src/main/java/com/iremdurmus/alertdialogkotlin/MainActivity.kt
+++ AlertDialogKotlin/app/src/main/java/com/iremdurmus/alertdialogkotlin/MainActivity.kt
@@ -1,11 +1,61 @@
 package com.iremdurmus.alertdialogkotlin
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 1) Toast 2) AlertDialog  3) Snackbar
+
+        //Context
+        //Activity Context -> this
+        //Application Context -> applicationContext
+
+        Toast.makeText(this@MainActivity,"welcome!",Toast.LENGTH_LONG).show()
+
+
+        //lambda
+        /*
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            println("button clicked")
+        }
+
+        button.setOnClickListener( object : View.OnClickListener
+        {
+            override fun onClick(p0: View?) {
+                println("")
+            }
+
+        })
+
+         */
     }
+
+    fun save (view : View)
+    {
+        val alert = AlertDialog.Builder(this@MainActivity)
+        alert.setTitle("Save")
+        alert.setMessage("Are you sure?")
+        alert.setPositiveButton("Yes", object : DialogInterface.OnClickListener{
+            override fun onClick(p0: DialogInterface?, p1: Int) {
+                Toast.makeText(this@MainActivity,"Saved!",Toast.LENGTH_LONG).show()
+            }
+        })
+        alert.setNegativeButton("No"){ p0, p1 ->
+            Toast.makeText(this@MainActivity,"Not Saved!",Toast.LENGTH_LONG).show()
+        }
+        alert.show()
+
+    }
+
 }
