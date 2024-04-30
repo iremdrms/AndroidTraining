deed50b2194be7ee062eae64ad7c65d0b82d1fe0
iremdrms <durmus.irem@outlook.com>
2024-04-30 11:15:38 +0300

    .

--- CountDownKotlin/app/src/main/java/com/iremdurmus/countdownkotlin/MainActivity.kt
+++ CountDownKotlin/app/src/main/java/com/iremdurmus/countdownkotlin/MainActivity.kt
@@ -2,10 +2,32 @@ package com.iremdurmus.countdownkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import com.iremdurmus.countdownkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //Abstract -Interface - Inheritance
+        object : CountDownTimer(10000,1000)
+        {
+            override fun onTick(p0: Long) {
+                binding.textView.text = "Left: ${p0 / 1000}" //p0 milisaniye olarak verildiği için
+            }
+
+            override fun onFinish() {
+                binding.textView.text = "bitti"
+            }
+
+        }.start()
+
+
+
     }
 }
